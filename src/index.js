84015be05d1d7bbc43d9503e953d90d8399d0806
Ugur Yilmaz <ugurbiyomek@gmail.com>
2022-12-14 15:08:30 +0300

    Add sortedArrayToBST function

--- src/index.js
+++ src/index.js
@@ -21,7 +21,22 @@ function buildTree(array) {
     return a - b;
   });
 
-  return arr;
+  return sortedArrayToBST(arr);
+}
+
+function sortedArrayToBST(arr) {
+  // base case
+  if (arr.length === 0) return null;
+
+  // get the middle element of the array and make it root
+  let middle = Math.floor(arr.length / 2);
+  let root = new Node(arr[middle]);
+
+  // recursively build left and right subtrees
+  root.right = sortedArrayToBST(arr.slice(0, middle));
+  root.left = sortedArrayToBST(arr.slice(middle + 1));
+
+  return root;
 }
 
 const a = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
