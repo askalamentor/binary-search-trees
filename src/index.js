14b292e5fd8c37c81de1243ca4dfbb78e55c01f9
Ugur Yilmaz <ugurbiyomek@gmail.com>
2022-12-14 16:42:23 +0300

    Update Tree class and add prettyPrint method

--- src/index.js
+++ src/index.js
@@ -1,43 +1,49 @@
 class Node {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
+  constructor(data = null, left = null, right = null) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
   }
 }
 
 class Tree {
   constructor(array) {
-    this.root = buildTree(array);
+    // remove duplicates and sort array
+    const arr = [...new Set(array)].sort((a, b) => a - b);
+    console.log(arr);
+    // build binary search tree
+    this.root = this.buildTree(arr);
+  }
+
+  buildTree(arr) {
+    // base case
+    if (arr.length === 0) return null;
+
+    // get the middle element of the array and make it root
+    let middle = Math.floor(arr.length / 2);
+    const root = new Node(arr[middle]);
+
+    // recursively build left and right subtrees
+    root.right = this.buildTree(arr.slice(0, middle));
+    root.left = this.buildTree(arr.slice(middle + 1));
+
+    return root;
+  }
+
+  prettyPrint(node = this.root, prefix = '', isLeft = true) {
+    if (node.right !== null) {
+      this.prettyPrint(
+        node.right,
+        `${prefix}${isLeft ? '│   ' : '    '}`,
+        false
+      );
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    if (node.left !== null) {
+      this.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
   }
 }
 
-function buildTree(array) {
-  // remove duplicates
-  const arr = [...new Set(array)];
-
-  // sort elements
-  arr.sort(function (a, b) {
-    return a - b;
-  });
-
-  return sortedArrayToBST(arr);
-}
-
-function sortedArrayToBST(arr) {
-  // base case
-  if (arr.length === 0) return null;
-
-  // get the middle element of the array and make it root
-  let middle = Math.floor(arr.length / 2);
-  let root = new Node(arr[middle]);
-
-  // recursively build left and right subtrees
-  root.right = sortedArrayToBST(arr.slice(0, middle));
-  root.left = sortedArrayToBST(arr.slice(middle + 1));
-
-  return root;
-}
-
-const a = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-console.log(a);
+let a = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+a.prettyPrint();
