7b7c9cce52412f9c2cb4dd31dd36306fed96ac0a
Ugur Yilmaz <ugurbiyomek@gmail.com>
2022-12-14 14:53:16 +0300

    Add buildTree method

--- src/index.js
+++ src/index.js
@@ -11,3 +11,18 @@ class Tree {
     this.root = buildTree(array);
   }
 }
+
+function buildTree(array) {
+  // remove duplicates
+  const arr = [...new Set(array)];
+
+  // sort elements
+  arr.sort(function (a, b) {
+    return a - b;
+  });
+
+  return arr;
+}
+
+const a = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+console.log(a);
