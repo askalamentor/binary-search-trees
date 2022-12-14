41b404363d1d567c24e1f9fafc420f80b2cba33d
Ugur Yilmaz <ugurbiyomek@gmail.com>
2022-12-14 17:33:55 +0300

    Create node.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
-class Node {
-  constructor(data = null, left = null, right = null) {
-    this.data = data;
-    this.left = left;
-    this.right = right;
-  }
-}
+import Node from './node';
 
 class Tree {
   constructor(array) {
